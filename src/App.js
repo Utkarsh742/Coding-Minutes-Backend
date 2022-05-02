ac0907525a8eeec53ecc66e84a189da37f298b22
Sarveshwar Shukla <sarveshwarshukla27@gmail.com>
2022-05-02 14:16:54 +0530

    Initial Backend Version

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactContact from "./component/ReactContact";
+
+const App = () => {
+  return (
+    <>
+      <ReactContact />
+    </>
+  )
+}
+
+export default App
